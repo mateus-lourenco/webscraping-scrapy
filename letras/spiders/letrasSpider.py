38e8f0dbc593337d6c02f056fbdbb85ca8218796
Mateus Lourenco <mateus.lourenco.95@gmail.com>
2019-05-07 18:20:35 -0300

    corrigindo captura de albuns

--- letras/spiders/letrasSpider.py
+++ letras/spiders/letrasSpider.py
@@ -14,49 +14,57 @@ class LetrasspiderSpider(Spider):
     def parse(self, response):
         genero = self.start_urls[1] + self.estilo
         artistas_genero = genero + '/todosartistas.html'
-        req = Request(url=artistas_genero, callback=self.todos_artistas)
-        yield req
+        request = Request(
+            url=artistas_genero, 
+            callback=self.todos_artistas
+        )
+
+        yield request
 
     def todos_artistas(self, response):
         todos_artistas = response.css('.cnt-list--col3 a')
+        for artista in todos_artistas:
+            url = self.start_urls[0] + artista.css('::attr(href)').get()
+            request = Request(
+                url=url, 
+                callback=self.artistas_validos
+            )
+
+            yield request
+
+    def artistas_validos(self, response):
+        artista_sel = response.css('.h3 a')
+        disc = artista_sel.css('::text').get()
         artista = {}
-        for item in todos_artistas:
-            nome = item.xpath('text()').get()
-            link = self.start_urls[0] + item.css('::attr(href)').get()
+        if disc != None and disc.lower() == 'discografia':
+            url = self.start_urls[0] + artista_sel.css('::attr(href)').get()
+            artista['nome'] = response.css('.cnt-head_title h1 ::text').get()
+            request = Request(
+                        url=url, 
+                        callback=self.albuns
+                    )
 
-            req = Request(url=link, callback=self.disc_parse)
-            artista['nome'] = nome
-            req.meta['artista'] = artista
-            yield req
+            request.meta['artista'] = artista
 
-    def disc_parse(self, response):
-        disc = self.start_urls[0] + response.css('.h3 a ::attr(href)').get()
-        yield {
-            Request(url=disc, callback=self.albuns)
-        }
+            yield request
 
     def albuns(self, response):
         albuns = response.css('.cnt-discografia_cd')
-        lancamentos = albuns.xpath('./span/text()').extract_first()
-        links_musica = albuns.xpath('../ol/li[not(contains(@class, "contrib"))]/a/@href')
+        album_dic = {}
+        for album in albuns:
+            album_dic['titulo_album'] = album.css('.cnt-discografia_info a ::text').get()
+            album_dic['ano'] = self.ano(album.css('.cnt-discografia_info span ::text').get())
+            album_dic['titulo_musica'] = album.css('li:not([class^="contrib"]) span ::text').get()
 
-        yield { 
-            'albuns' : albuns,
-            'lancamentos' : lancamentos
-        }
+            yield album_dic
+            '''request = Request(
+                url= self.start_urls[0] + album.css('li:not([class^="contrib"]) ::attr(href)').get(),
+                callback= self.musicas
+            )
 
-        album = {}
+            request.meta['artista']['album'] = album_dic
 
-        for titulo, ano, link in zip(albuns, lancamentos, links_musica):
-            url = self.start_urls[0] + link
-            req = Request(url=url, callback=self.musicas)
-
-            album['titulo'] = titulo
-            album['ano'] = ano
-
-            req.meta['album'] = album
-
-            yield req
+            yield request'''
 
     '''def musicas(self, response):
         info_trad = response.css('div.letra-menu > a')
@@ -80,3 +88,5 @@ class LetrasspiderSpider(Spider):
             yield '' 
 
             '''
+    def ano(self, text):
+        return re.match(r'.*([1-2][0-9]{3})', text).group(1)
