7a2e485bfea3398d9b0039b4a7469f6b160f2cd3
Mateus Cunha Lourenço <mateus.lourenco@conductor.com.br>
2019-05-10 11:55:01 -0300

    criando item

--- letras/items.py
+++ letras/items.py
@@ -5,10 +5,11 @@
 # See documentation in:
 # https://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy import Item, Field
 
-
-class LetrasItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class Musica(Item):    
+    artista = Field()
+    album = Field()
+    titulo = Field()
+    compositor = Field()
+    letra = Field()
--- letras/spiders/letrasSpider.py
+++ letras/spiders/letrasSpider.py
@@ -1,38 +1,38 @@
 # -*- coding: utf-8 -*-
 from scrapy import Request, Spider
+from letras.items import Musica
 import re
 
-class LetrasspiderSpider(Spider):
+class LetrasSpider(Spider):
     name = 'letrasSpider'
     allowed_domains = ['www.letras.mus.br']
     start_urls = [
         'https://www.letras.mus.br',
         'https://www.letras.mus.br/estilos/'
     ]
-    estilo = ''
 
     def parse(self, response):
-        genero = self.start_urls[1] + self.estilo
+        genero = self.start_urls[1] + estilo
         artistas_genero = genero + '/todosartistas.html'
         request = Request(
             url=artistas_genero, 
-            callback=self.todos_artistas
+            callback=self.parse_artista
         )
 
         yield request
 
-    def todos_artistas(self, response):
+    def parse_artista(self, response):
         todos_artistas = response.css('.cnt-list--col3 a')
         for artista in todos_artistas:
             url = self.start_urls[0] + artista.css('::attr(href)').get()
             request = Request(
                 url=url, 
-                callback=self.artistas_validos
+                callback=self.parse_artista_valido
             )
 
             yield request
 
-    def artistas_validos(self, response):
+    def parse_artista_valido(self, response):
         disc = response.css('.artista-albuns .cnt-more').get()
         if disc is not None:
             url = self.start_urls[0] + response.css('.h3 a ::attr(href)').get()
@@ -40,32 +40,34 @@ class LetrasspiderSpider(Spider):
             
             request = Request(
                         url=url, 
-                        callback=self.albuns
+                        callback=self.parse_album
                     )
 
             request.meta['nome'] = nome
             yield request
 
-    def albuns(self, response):
+    def parse_album(self, response):
         albuns = response.css('.cnt-discografia_cd')                  
         album_dic = {}
         for album in albuns:
             url_musica = album.css('li:not([class^="contrib"]) ::attr(href)').get()
             album_dic = {
-                    'nome': response.meta['nome'],
-                    'album' : {   
-                        'titulo_album' : album.css('.cnt-discografia_info a ::text').get(),
-                        'ano' : self.ano(album.css('.cnt-discografia_info span ::text').get())                    }
+                'nome': response.meta['nome'],
+                'album' : {   
+                   'titulo_album' : album.css('.cnt-discografia_info a ::text').get(),
+                   'ano' : album.css('.cnt-discografia_info span ::text').re(r'.*([1-2][0-9]{3})').group(1)
                 }
+            }
+            
             request = Request(
                 url=self.start_urls[0] + url_musica,
-                callback=self.musicas
+                callback=self.parse_musica
             )           
             request.meta['artista'] = album_dic
 
             yield request
 
-    def musicas(self, response):
+    def parse_musica(self, response):
         trad = response.css('.letra-menu a ::attr(data-tt)').get()
 
         musica = {}
@@ -74,12 +76,9 @@ class LetrasspiderSpider(Spider):
                 'artista': response.meta['artista']['nome'],
                 'album': response.meta['artista']['album'],
                 'titulo' : response.css('.cnt-head_title h1 ::text').get(),
-                'compositor(es)' : response.css('.letra-info_comp ::text').get(),
+                'compositor' : response.css('.letra-info_comp ::text').get(),
                 'letra' : ''.join(verso + '\r\n' for verso in response.css('div.cnt-letra p ::text').getall())
             }
 
+            #yield Musica(musica)
             yield musica
-
-
-    def ano(self, text):
-        return re.match(r'.*([1-2][0-9]{3})', text).group(1)
